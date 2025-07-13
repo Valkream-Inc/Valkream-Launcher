9447df17f4b87cb0596633c778636219d48dd833
Therin Boris <89749441+BorisTherin@users.noreply.github.com>
2025-07-13 17:36:15 +0200

    added serverinfos constants & server maintenance state

---
+++ frontend-electron-launcher/src/shared/constants/serverInfos.js
@@ -0,0 +1,12 @@
+
+const serverInfos = {
+    ip: '51.222.46.122',
+    port: 2457,
+    timeout: 3000,
+    maintenance: [  // UTC 0 TIME PLZ
+        { hour: 15, minute: 5},
+        { hour: 4, minute: 0}        
+    ]
+}
+
+module.exports = serverInfos;
